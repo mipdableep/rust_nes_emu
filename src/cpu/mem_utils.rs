e1d0aea57f5c10a818d1fc674768c094df4d97f2
Tomer Pelleg <tomer@pelleg.org>
2024-09-09 22:00:18 +0300

    mem_utils: added memory reading functions

--- src/cpu/mem_utils.rs
+++ src/cpu/mem_utils.rs
@@ -1,3 +1,5 @@
+use crate::cpu::CPU;
+
 #[derive(Debug)]
 #[allow(non_camel_case_types)]
 pub enum AddressingMode {
@@ -17,3 +19,20 @@ pub enum AddressingMode {
     Relative,
     Indirect,
 }
+
+impl CPU {
+    fn read_memory(&self, addr: u16) -> u8 {
+        self.memory[addr as usize]
+    }
+
+    fn read_memory_2_bytes(&self, addr: u16) -> u16 {
+        let low = self.mem_read(addr) as u16;
+        let high = self.mem_read(addr + 1) as u16;
+        (high << 8) | (low as u16)
+    }
+
+    fn read_memory_2_bytes_with_overflow(&self, addr: u16) -> u16 {
+        let low = self.mem_read(addr) as u16;
+        let high = self.mem_read(addr.wrapping_add(1)) as u16;
+        (high << 8) | (low as u16)
+    }
