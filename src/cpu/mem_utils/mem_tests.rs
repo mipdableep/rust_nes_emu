498d78086af2626a4861f5b707a1a9b26a8384b3
Ido Talmor <ifw.talmor@gmail.com>
2024-09-14 01:24:19 +0300

    test(mem_tests---zeroPage_X/Y): added tests for default and wrapping

--- src/cpu/mem_utils/mem_tests.rs
+++ src/cpu/mem_utils/mem_tests.rs
@@ -43,3 +43,49 @@ fn mode_to_mem_addr_zeropage() {
     assert_eq!(15, cpu.convert_mode_to_val(AddressingMode::ZeroPage));
     cpu.program_counter += 1;
 }
+
+#[test]
+fn mode_to_mem_zeropage_x() {
+    let mut cpu = CPU::new();
+    cpu.memory[11] = 24;
+    cpu.memory[13] = 21;
+    cpu.memory[20] = 15;
+    cpu.register_x = 1;
+
+    let program = vec![10, 15, 17];
+    cpu.load(program);
+
+    assert_eq!(24, cpu.convert_mode_to_val(AddressingMode::ZeroPage_X));
+    cpu.program_counter += 1;
+    // test the wraping
+    cpu.register_x = 254;
+    assert_eq!(21, cpu.convert_mode_to_val(AddressingMode::ZeroPage_X));
+    cpu.program_counter += 1;
+
+    cpu.register_x = 3;
+    assert_eq!(15, cpu.convert_mode_to_val(AddressingMode::ZeroPage_X));
+    cpu.program_counter += 1;
+}
+
+#[test]
+fn mode_to_mem_zeropage_y() {
+    let mut cpu = CPU::new();
+    cpu.memory[11] = 24;
+    cpu.memory[13] = 21;
+    cpu.memory[20] = 15;
+    cpu.register_y = 1;
+
+    let program = vec![10, 15, 17];
+    cpu.load(program);
+
+    assert_eq!(24, cpu.convert_mode_to_val(AddressingMode::ZeroPage_Y));
+    cpu.program_counter += 1;
+    // test the wraping
+    cpu.register_y = 254;
+    assert_eq!(21, cpu.convert_mode_to_val(AddressingMode::ZeroPage_Y));
+    cpu.program_counter += 1;
+
+    cpu.register_y = 3;
+    assert_eq!(15, cpu.convert_mode_to_val(AddressingMode::ZeroPage_Y));
+    cpu.program_counter += 1;
+}
