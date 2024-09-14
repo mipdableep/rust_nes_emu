c3db65d5e09330da9c433411585c23cc82458dd2
Tomer Pelleg <tomer@pelleg.org>
2024-09-14 18:57:00 +0300

    read_memory: changed to public

--- src/cpu/mem_utils.rs
+++ src/cpu/mem_utils.rs
@@ -24,7 +24,7 @@ pub enum AddressingMode {
 
 #[allow(dead_code)]
 impl CPU {
-    fn read_memory(&self, addr: u16) -> u8 {
+    pub fn read_memory(&self, addr: u16) -> u8 {
         self.memory[addr as usize]
     }
 
