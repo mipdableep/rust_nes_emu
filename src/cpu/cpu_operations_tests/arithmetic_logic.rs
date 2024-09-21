4ba57d0bb56d6aaf09d0f75fb7fef2a9c55f2eee
Tomer Pelleg <tomer@pelleg.org>
2024-09-21 13:58:35 +0300

    ADC: fixed ignoring the carry while computing

--- src/cpu/cpu_operations_tests/arithmetic_logic.rs
+++ src/cpu/cpu_operations_tests/arithmetic_logic.rs
@@ -1,7 +1,8 @@
 use crate::cpu::CPU;
 
-fn set_add_test(cpu: &mut CPU, reg_a: u8, operand: u8, should_overflow: bool, should_carry: bool) {
-    let expected_result = operand.wrapping_add(reg_a);
+fn set_add_test(cpu: &mut CPU, reg_a: u8, operand: u8, should_overflow: bool, should_carry: bool, old_carry: bool) {
+    let expected_result = operand.wrapping_add(reg_a).wrapping_add(match old_carry { true => 1, false => 0});
+    cpu.set_carry(old_carry);
     cpu.register_a = reg_a;
     cpu.ADC(operand);
     assert_eq!(cpu.register_a, expected_result);
@@ -15,18 +16,34 @@ fn set_add_test(cpu: &mut CPU, reg_a: u8, operand: u8, should_overflow: bool, sh
 #[allow(non_snake_case)]
 fn ADC() {
     let mut cpu: CPU = CPU::new();
-    set_add_test(&mut cpu, 5, 3, false, false);
-    set_add_test(&mut cpu, 0, 0, false, false);
-    set_add_test(&mut cpu, 1, 0xFF, false, true);
-    set_add_test(&mut cpu, 0xFF, 0xFF, false, true);
-    set_add_test(&mut cpu, 0xa5, 0x84, true, true);
-    set_add_test(&mut cpu, 0x39, 0xde, false, true);
-    set_add_test(&mut cpu, 0xdd, 0x0a, false, false);
-    set_add_test(&mut cpu, 0xc8, 0x53, false, true);
-    set_add_test(&mut cpu, 0x5b, 0xde, false, true);
-    set_add_test(&mut cpu, 0x5b, 0xde, false, true);
-    set_add_test(&mut cpu, 0x8c, 0x75, false, true);
-    set_add_test(&mut cpu, 0x04, 0x27, false, false);
+    set_add_test(&mut cpu, 5, 3, false, false, false);
+    set_add_test(&mut cpu, 0, 0, false, false, false);
+    set_add_test(&mut cpu, 1, 0xFF, false, true, false);
+    set_add_test(&mut cpu, 0xFF, 0xFF, false, true, false);
+    set_add_test(&mut cpu, 0x40, 0x40, true, false, false);
+    set_add_test(&mut cpu, 0x3f, 0x40, false, false, false);
+
+    set_add_test(&mut cpu, 0xa5, 0x84, true, true, false);
+    set_add_test(&mut cpu, 0x39, 0xde, false, true, false);
+    set_add_test(&mut cpu, 0xdd, 0x0a, false, false, false);
+    set_add_test(&mut cpu, 0xc8, 0x53, false, true, false);
+    set_add_test(&mut cpu, 0x5b, 0xde, false, true, false);
+    set_add_test(&mut cpu, 0x5b, 0xde, false, true, false);
+    set_add_test(&mut cpu, 0x8c, 0x75, false, true, false);
+    set_add_test(&mut cpu, 0x04, 0x27, false, false, false);
+
+    set_add_test(&mut cpu, 5, 3, false, false, true);
+    set_add_test(&mut cpu, 0, 0, false, false, true);
+    set_add_test(&mut cpu, 1, 0xFF, false, true, true);
+    set_add_test(&mut cpu, 0xFF, 0xFF, false, true, true);
+    set_add_test(&mut cpu, 0xa5, 0x84, true, true, true);
+    set_add_test(&mut cpu, 0x39, 0xde, false, true, true);
+    set_add_test(&mut cpu, 0xdd, 0x0a, false, false, true);
+    set_add_test(&mut cpu, 0x00, 0xFF, false, true, true);
+    set_add_test(&mut cpu, 0x080, 0x7f, false, true, true);
+    set_add_test(&mut cpu, 0x40, 0x40, true, false, true);
+    set_add_test(&mut cpu, 0x3f, 0x40, true, false, true);
+
 }
 
 fn get_random_u8_pairs() -> Vec<[u8; 2]> {
