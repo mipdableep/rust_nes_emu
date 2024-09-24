56e3e6ee64efc79e175a60460e701f8f0ee1ddae
Tomer Pelleg <tomer@pelleg.org>
2024-09-24 21:39:25 +0300

    control_flow: removed unnecessary code (two's complement conversion), since it happens in the addressing mode resolving
    Also removed some deprecated allow dead_code

--- src/cpu/cpu_operations_tests/control_flow.rs
+++ src/cpu/cpu_operations_tests/control_flow.rs
@@ -1,16 +1,4 @@
-use cpu::cpu_operations::control_flow::convert_u8_to_i8_2sc;
-use crate::cpu;
 use crate::cpu::CPU;
-
-#[test]
-fn test_2s_conversion(){
-    assert_eq!(convert_u8_to_i8_2sc(0), 0);
-    assert_eq!(convert_u8_to_i8_2sc(0xff), -1);
-    assert_eq!(convert_u8_to_i8_2sc(0x7f), 127);
-    assert_eq!(convert_u8_to_i8_2sc(0x80), -128);
-    assert_eq!(convert_u8_to_i8_2sc(0xa4), -0x5c);
-    assert_eq!(convert_u8_to_i8_2sc(0x5f), 0x5f);
-}
 
 #[test]
 #[allow(non_snake_case)]
