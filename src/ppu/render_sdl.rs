9d3908c539c05234960339fe92fc65a6e9d03afa
Tomer Pelleg <tomer@pelleg.org>
2024-11-16 00:23:47 +0200

    fix(screen factor): 10 is too big for my laptop screen, 2 is plenty enough

--- src/ppu/render_sdl.rs
+++ src/ppu/render_sdl.rs
@@ -2,11 +2,9 @@ use screen_rendering_constants::*;
 use sdl2::render::{Texture, WindowCanvas};
 
 pub(crate) mod screen_rendering_constants {
-    pub const SCREEN_WIDTH: usize = 240;
-    pub const SCREEN_HEIGHT: usize = 260;
-    pub const SCREEN_FACTOR: usize = 10;
     pub const SCREEN_WIDTH: usize = 256;
     pub const SCREEN_HEIGHT: usize = 240;
+    pub const SCREEN_FACTOR: usize = 2;
 }
 
 #[derive(Debug, Eq, PartialEq)]
