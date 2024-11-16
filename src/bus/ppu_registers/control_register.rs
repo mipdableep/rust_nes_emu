15b2d7cecd60cff247b0c4330213774ccbf457ab
Tomer Pelleg <tomer@pelleg.org>
2024-11-16 13:54:01 +0200

    docs(control register): fixed the comment explaining what each bit is doing

--- src/bus/ppu_registers/control_register.rs
+++ src/bus/ppu_registers/control_register.rs
@@ -1,20 +1,21 @@
 #[derive(Debug, Eq, PartialEq)]
 pub struct PPUControlRegister {
-    misc_settings: u8, // 7  bit  0
-                       // ---- ----
-                       // VPHB SINN
-                       // |||| ||||
-                       // |||| ||++- Base nametable address
-                       // |||| ||    (0 = $2000; 1 = $2400; 2 = $2800; 3 = $2C00)
-                       // |||| |+--- VRAM address increment per CPU read/write of PPUDATA
-                       // |||| |     (0: add 1, going across; 1: add 32, going down)
-                       // |||| +---- Sprite pattern table address for 8x8 sprites
-                       // ||||       (0: $0000; 1: $1000; ignored in 8x16 mode)
-                       // |||+------ Background pattern table address (0: $0000; 1: $1000)
-                       // ||+------- Sprite size (0: 8x8 pixels; 1: 8x16 pixels – see PPU OAM#Byte 1)
-                       // |+-------- PPU master/slave select
-                       // |          (0: read backdrop from EXT pins; 1: output color on EXT pins)
-                       // +--------- Vblank NMI enable (0: off, 1: on)
+    misc_settings: u8,
+    // 7  bit  0
+    // ---- ----
+    // VPHB SINN
+    // |||| ||||
+    // |||| ||++- Base nametable address
+    // |||| ||    (0 = $2000; 1 = $2400; 2 = $2800; 3 = $2C00)
+    // |||| |+--- VRAM address increment per CPU read/write of PPUDATA
+    // |||| |     (0: add 1, going across; 1: add 32, going down)
+    // |||| +---- Sprite pattern table address for 8x8 sprites
+    // ||||       (0: $0000; 1: $1000; ignored in 8x16 mode)
+    // |||+------ Background pattern table address (0: $0000; 1: $1000)
+    // ||+------- Sprite size (0: 8x8 pixels; 1: 8x16 pixels – see PPU OAM#Byte 1)
+    // |+-------- PPU master/slave select
+    // |          (0: read backdrop from EXT pins; 1: output color on EXT pins)
+    // +--------- Vblank NMI enable (0: off, 1: on)
 }
 
 impl PPUControlRegister {
