46f20015726719fbe9f94b143b288ce149118df2
Tomer Pelleg <tomer@pelleg.org>
2024-11-23 00:22:48 +0200

    feat(nes_main): added very basic game loop!

--- nes_main/main.rs
+++ nes_main/main.rs
@@ -1,46 +1,53 @@
 use std::{thread::sleep, time};
 
-use nes_emulator::{generate_cpu, generate_texture_canvas_event_pump};
-
 use nes_emulator::ppu::render_sdl::{
     screen_rendering_constants::{SCREEN_HEIGHT, SCREEN_WIDTH},
     update_texture_from_frame, Frame,
 };
+use nes_emulator::ppu::PPU;
+use nes_emulator::prelude::{Bus, CPU};
+use nes_emulator::{generate_cpu, generate_texture_canvas_event_pump};
 
 fn main() {
-    let mut frame = Frame::new();
-    for x in 0..SCREEN_WIDTH {
-        for y in 0..SCREEN_HEIGHT {
-            match (x < SCREEN_WIDTH / 2, y < SCREEN_HEIGHT / 2) {
-                (true, true) => frame.set_pixel(x, y, (128, 0, 0)),
-                (true, false) => frame.set_pixel(x, y, (0, 128, 0)),
-                (false, true) => frame.set_pixel(x, y, (0, 0, 128)),
-                (false, false) => frame.set_pixel(x, y, (255, 255, 255)),
-            }
-        }
-    }
+    let mut bus: Bus = Bus::new();
+    let bytes = std::fs::read("./roms/pacman.nes").unwrap();
+    bus.cartridge.load_from_dump(&bytes);
 
-    generate_cpu!(cpu);
+    let mut bus_ref = &mut bus;
+
+    let mut cpu = CPU::new(bus_ref);
+    cpu.program_counter = cpu.read_memory_2_bytes(0xFFFC); //TODO: create cpu.reset or something?
+    bus_ref = cpu.bus.take().unwrap();
 
     generate_texture_canvas_event_pump!(texture, canvas, event_pump);
+    let mut frame = Frame::new();
+    let mut ppu = PPU::new(bus_ref);
 
-    update_texture_from_frame(&mut texture, &mut frame, &mut canvas);
     loop {
-        canvas.present();
+        bus_ref = ppu.bus.take().unwrap();
+        cpu.bus = Some(bus_ref);
+        cpu.run_one_cycle();
 
-        //handle events
-        while let Some(event) = event_pump.poll_event() {
-            match event {
-                Event::Quit { .. } => std::process::exit(0),
-                Event::KeyDown {
-                    keycode: Some(Keycode::Escape),
-                    ..
-                } => std::process::exit(0),
-                _ => {}
-            }
+        bus_ref = cpu.bus.take().unwrap();
+        ppu.bus = Some(bus_ref);
+        ppu.run_one_ppu_cycle(&mut texture, &mut frame, &mut canvas);
+        ppu.run_one_ppu_cycle(&mut texture, &mut frame, &mut canvas);
+        ppu.run_one_ppu_cycle(&mut texture, &mut frame, &mut canvas);
+
+        let sleep_time = time::Duration::from_secs_f64(0.001);
+        // sleep(sleep_time);
+
+        let event = match event_pump.poll_event() {
+            Some(i) => i,
+            None => continue,
+        };
+        match event {
+            Event::Quit { .. }
+            | Event::KeyDown {
+                keycode: Some(Keycode::Escape),
+                ..
+            } => std::process::exit(0),
+            _ => {}
         }
-
-        let sleep_time = time::Duration::from_secs_f64(0.2);
-        sleep(sleep_time);
     }
 }
