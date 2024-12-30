9c775ae7955298ff8a5f1be0cf54024a46516861
Tomer Pelleg <tomer@pelleg.org>
2024-12-30 21:10:47 +0200

    feat(oam address register): added ppu register for oam address (basically characters sprites)

--- src/bus/ppu_registers/mod.rs
+++ src/bus/ppu_registers/mod.rs
@@ -2,6 +2,7 @@ use crate::bus::ppu_registers::address_register::PPUAddressReg;
 use crate::bus::ppu_registers::control_register::PPUControlRegister;
 use crate::bus::ppu_registers::data_register::PPUDataReg;
 use crate::bus::ppu_registers::mask_register::PPUMaskRegister;
+use crate::bus::ppu_registers::oam_address_register::OAMAdressRegister;
 use crate::bus::ppu_registers::scroll_register::PPUScrollReg;
 use crate::bus::ppu_registers::status_register::PPUStatusRegister;
 
@@ -9,6 +10,7 @@ pub mod address_register;
 pub mod control_register;
 mod data_register;
 mod mask_register;
+mod oam_address_register;
 mod scroll_register;
 mod status_register;
 
@@ -17,6 +19,7 @@ pub struct PPURegisters {
     pub control_register: PPUControlRegister,
     pub mask_register: PPUMaskRegister,
     pub status_register: PPUStatusRegister,
+    pub oam_addr_register: OAMAdressRegister,
     pub address_register: PPUAddressReg,
     pub data_register: PPUDataReg,
     pub scroll_register: PPUScrollReg,
@@ -28,6 +31,7 @@ impl PPURegisters {
             control_register: PPUControlRegister::new(),
             mask_register: PPUMaskRegister::new(),
             status_register: PPUStatusRegister::new(),
+            oam_addr_register: OAMAdressRegister::new(),
             address_register: PPUAddressReg::new(),
             data_register: PPUDataReg::new(),
             scroll_register: PPUScrollReg::new(),
---
+++ src/bus/ppu_registers/oam_address_register.rs
@@ -0,0 +1,26 @@
+use std::ops::Deref;
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct OAMAdressRegister(u8);
+
+impl Deref for OAMAdressRegister {
+    type Target = u8;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl OAMAdressRegister {
+    pub fn new() -> Self {
+        Self(0)
+    }
+
+    pub fn read_current_value(&self) -> u8 {
+        self.0
+    }
+
+    pub fn update_current_value(&mut self, new_value: u8) {
+        self.0 = new_value
+    }
+}
