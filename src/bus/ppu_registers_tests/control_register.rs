e2a5170b1728d6a2e45b92adc9d286945034b858
Tomer Pelleg <tomer@pelleg.org>
2025-01-11 14:33:55 +0200

    refactor(control register tests): renamed the variable from ppu_addr_reg to ppu_ctrl_reg, since it is the control register

--- src/bus/ppu_registers_tests/control_register.rs
+++ src/bus/ppu_registers_tests/control_register.rs
@@ -2,32 +2,32 @@ use crate::bus::ppu_registers::control_register::PPUControlRegister;
 
 #[test]
 fn test_read_and_write() {
-    let mut ppu_addr_reg = PPUControlRegister::new();
-    assert_eq!(ppu_addr_reg.read(), 0);
-    ppu_addr_reg.write_byte(0x34);
-    assert_eq!(ppu_addr_reg.read(), 0x34);
+    let mut ppu_ctrl_reg = PPUControlRegister::new();
+    assert_eq!(ppu_ctrl_reg.read(), 0);
+    ppu_ctrl_reg.write_byte(0x34);
+    assert_eq!(ppu_ctrl_reg.read(), 0x34);
 }
 
 #[test]
 fn test_get_vblank_nmi() {
-    let mut ppu_addr_reg = PPUControlRegister::new();
-    assert!(!ppu_addr_reg.get_vblank_nmi());
-    ppu_addr_reg.write_byte(0x80);
-    assert!(ppu_addr_reg.get_vblank_nmi());
-    ppu_addr_reg.write_byte(0x7F);
-    assert!(!ppu_addr_reg.get_vblank_nmi());
-    ppu_addr_reg.write_byte(0x12);
-    assert!(!ppu_addr_reg.get_vblank_nmi());
+    let mut ppu_ctrl_reg = PPUControlRegister::new();
+    assert!(!ppu_ctrl_reg.get_vblank_nmi());
+    ppu_ctrl_reg.write_byte(0x80);
+    assert!(ppu_ctrl_reg.get_vblank_nmi());
+    ppu_ctrl_reg.write_byte(0x7F);
+    assert!(!ppu_ctrl_reg.get_vblank_nmi());
+    ppu_ctrl_reg.write_byte(0x12);
+    assert!(!ppu_ctrl_reg.get_vblank_nmi());
 }
 
 #[test]
 fn test_get_vram_address_increment() {
-    let mut ppu_addr_reg = PPUControlRegister::new();
-    assert_eq!(ppu_addr_reg.get_vram_address_inc(), 1);
-    ppu_addr_reg.write_byte(0x04);
-    assert_eq!(ppu_addr_reg.get_vram_address_inc(), 32);
-    ppu_addr_reg.write_byte(0xFB);
-    assert_eq!(ppu_addr_reg.get_vram_address_inc(), 1);
-    ppu_addr_reg.write_byte(0x12);
-    assert_eq!(ppu_addr_reg.get_vram_address_inc(), 1);
+    let mut ppu_ctrl_reg = PPUControlRegister::new();
+    assert_eq!(ppu_ctrl_reg.get_vram_address_inc(), 1);
+    ppu_ctrl_reg.write_byte(0x04);
+    assert_eq!(ppu_ctrl_reg.get_vram_address_inc(), 32);
+    ppu_ctrl_reg.write_byte(0xFB);
+    assert_eq!(ppu_ctrl_reg.get_vram_address_inc(), 1);
+    ppu_ctrl_reg.write_byte(0x12);
+    assert_eq!(ppu_ctrl_reg.get_vram_address_inc(), 1);
 }
