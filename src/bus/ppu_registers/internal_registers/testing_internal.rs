03b1d60f70da608f6b750861c688c28595e0e8d4
Tomer Pelleg <tomer@pelleg.org>
2025-01-15 12:07:26 +0200

    tests(internal ppu registers): added tests for the internal ppu registers behaviours when writing to various ppu registers

--- src/bus/ppu_registers/internal_registers/testing_internal.rs
+++ src/bus/ppu_registers/internal_registers/testing_internal.rs
@@ -1,8 +1,13 @@
 use crate::{bus_mut, generate_cpu};
 
+macro_rules! ppu_regs {
+    ($cpu: ident) => {
+        bus_mut!($cpu).ppu_registers
+    };
+}
 macro_rules! inter_regs {
     ($cpu: ident) => {
-        bus_mut!($cpu).ppu_registers.internal_registers
+        ppu_regs!($cpu).internal_registers
     };
 }
 
@@ -22,4 +27,44 @@ fn test_read_status_clear_latch() {
     assert_eq!(inter_regs!(cpu).write_toggle, false);
     cpu.read_memory(0x2002);
     assert_eq!(inter_regs!(cpu).write_toggle, false);
+}
+
+#[test]
+// based on https://www.nesdev.org/wiki/PPU_scrolling#Summary
+fn test_internal_full() {
+    generate_cpu!(cpu);
+    let old_v: u16 = 0b1010110_10111100;
+    let old_t: u16 = 0b1001100_00010010;
+    inter_regs!(cpu).current_vram = old_v;
+    inter_regs!(cpu).temporary_vram = old_t;
+    inter_regs!(cpu).write_toggle = true;
+
+    cpu.write_memory(0x2000, 0);
+    assert_eq!(inter_regs!(cpu).temporary_vram, 0b1000000_00010010);
+    assert_eq!(inter_regs!(cpu).current_vram, old_v);
+
+    cpu.read_memory(0x2002);
+    assert_eq!(inter_regs!(cpu).write_toggle, false);
+
+    cpu.write_memory(0x2005, 0b01111101);
+    assert_eq!(inter_regs!(cpu).write_toggle, true);
+    assert_eq!(inter_regs!(cpu).temporary_vram, 0b1000000_00001111);
+    assert_eq!(inter_regs!(cpu).fine_x, 0b101);
+    assert_eq!(inter_regs!(cpu).current_vram, old_v);
+
+    cpu.write_memory(0x2005, 0b01011110);
+    assert_eq!(inter_regs!(cpu).write_toggle, false);
+    assert_eq!(inter_regs!(cpu).temporary_vram, 0b1100001_01101111);
+    assert_eq!(inter_regs!(cpu).current_vram, old_v);
+
+    cpu.write_memory(0x2006, 0b00111101);
+    assert_eq!(inter_regs!(cpu).write_toggle, true);
+    assert_eq!(inter_regs!(cpu).temporary_vram, 0b0111101_01101111);
+    assert_eq!(inter_regs!(cpu).current_vram, old_v);
+
+    cpu.write_memory(0x2006, 0b11110000);
+    assert_eq!(inter_regs!(cpu).write_toggle, false);
+    assert_eq!(inter_regs!(cpu).temporary_vram, 0b0111101_11110000);
+    assert_eq!(inter_regs!(cpu).current_vram, 0b0111101_11110000);
+    assert_eq!(inter_regs!(cpu).fine_x, 0b101);
 }
