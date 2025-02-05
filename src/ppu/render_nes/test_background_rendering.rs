b6b4ba80f0a3fc7b95bf9d2da0ec998adeb61ab4
Tomer Pelleg <tomer@pelleg.org>
2025-02-05 16:30:16 +0200

    refactor(background drawing): created a function to return the tile, and to get the color for the tile for the assertion afterward

--- src/ppu/render_nes/test_background_rendering.rs
+++ src/ppu/render_nes/test_background_rendering.rs
@@ -6,6 +6,50 @@ use crate::prelude::Mirroring;
 use crate::{bus_mut, generate_ppu, generate_texture_canvas_event_pump};
 use serial_test::serial;
 
+fn get_half_tile() -> [u8; 16] {
+    // Bit Planes            Pixel Pattern
+    // $0xx0=$41  01000001
+    // $0xx1=$C2  11000010
+    // $0xx2=$44  01000100
+    // $0xx3=$48  01001000
+    // $0xx4=$10  00010000
+    // $0xx5=$20  00100000         .1.....3
+    // $0xx6=$40  01000000         11....3.
+    // $0xx7=$80  10000000  =====  .1...3..
+    //     .1..3...
+    // $0xx8=$01  00000001  =====  ...3.22.
+    // $0xx9=$02  00000010         ..3....2
+    // $0xxA=$04  00000100         .3....2.
+    // $0xxB=$08  00001000         3....222
+    // $0xxC=$16  00010110
+    // $0xxD=$21  00100001
+    // $0xxE=$42  01000010
+    // $0xxF=$87  10000111
+
+    [
+        0x41_u8, 0xC2, 0x44, 0x48, 0x10, 0x20, 0x40, 0x80, 0x01, 0x02, 0x04, 0x08, 0x16, 0x21,
+        0x42, 0x87,
+    ]
+}
+
+fn get_color_index_for_half_tile(x: i32, y: i32) -> usize {
+    // gets coordinates of a point, and return the expected color index according to
+    // $0xx5=$20  00100000         .1.....3
+    // $0xx6=$40  01000000         11....3.
+    // $0xx7=$80  10000000  =====  .1...3..
+    //                             .1..3...
+    // $0xx8=$01  00000001  =====  ...3.22.
+    // $0xx9=$02  00000010         ..3....2
+    // $0xxA=$04  00000100         .3....2.
+    // $0xxB=$08  00001000         3....222
+    match (x, y) {
+        (0, 1) | (1, 1) | (1, 0) | (1, 2) | (1, 3) => 1,
+        (7, 7) | (6, 7) | (5, 7) | (6, 6) | (7, 5) | (5, 4) | (6, 4) => 2,
+        (0, 7) | (1, 6) | (2, 5) | (3, 4) | (4, 3) | (5, 2) | (6, 1) | (7, 0) => 3,
+        _ => 0,
+    }
+}
+
 #[test]
 #[serial]
 fn background_drawing() {
@@ -16,31 +60,7 @@ fn background_drawing() {
 
     bus_mut!(ppu).cartridge.screen_mirroring = Mirroring::Horizontal;
 
-    { // set tile 7 to be our tile
-         // this has its own scope to be collapsable in the IDE
-         // Bit Planes            Pixel Pattern
-         // $0xx0=$41  01000001
-         // $0xx1=$C2  11000010
-         // $0xx2=$44  01000100
-         // $0xx3=$48  01001000
-         // $0xx4=$10  00010000
-         // $0xx5=$20  00100000         .1.....3
-         // $0xx6=$40  01000000         11....3.
-         // $0xx7=$80  10000000  =====  .1...3..
-         //     .1..3...
-         // $0xx8=$01  00000001  =====  ...3.22.
-         // $0xx9=$02  00000010         ..3....2
-         // $0xxA=$04  00000100         .3....2.
-         // $0xxB=$08  00001000         3....222
-         // $0xxC=$16  00010110
-         // $0xxD=$21  00100001
-         // $0xxE=$42  01000010
-         // $0xxF=$87  10000111
-    }
-    let our_tile = [
-        0x41_u8, 0xC2, 0x44, 0x48, 0x10, 0x20, 0x40, 0x80, 0x01, 0x02, 0x04, 0x08, 0x16, 0x21,
-        0x42, 0x87,
-    ];
+    let our_tile = get_half_tile();
     bus_mut!(ppu).cartridge.chr_rom = vec![0; 0x2000];
     bus_mut!(ppu).cartridge.chr_rom[7 * 16..8 * 16].copy_from_slice(&our_tile);
 
@@ -53,34 +73,27 @@ fn background_drawing() {
     bus_mut!(ppu).ppu_memory.palette_table[14] = 19;
     bus_mut!(ppu).ppu_memory.palette_table[15] = 24;
 
+    let wanted_palette = [
+        SYSTEM_PALETTE[0],
+        SYSTEM_PALETTE[3],
+        SYSTEM_PALETTE[19],
+        SYSTEM_PALETTE[24],
+    ];
+
     let mut frame = Frame::new();
     for _ in 0..SCANLINE_LENGTH_PIXELS {
         for _ in 0..SCANLINES_PER_FRAME {
             ppu.run_one_ppu_cycle(&mut texture, &mut frame, &mut canvas, &mut event_pump);
         }
     }
-    // $0xx5=$20  00100000         .1.....3
-    // $0xx6=$40  01000000         11....3.
-    // $0xx7=$80  10000000  =====  .1...3..
-    //                             .1..3...
-    // $0xx8=$01  00000001  =====  ...3.22.
-    // $0xx9=$02  00000010         ..3....2
-    // $0xxA=$04  00000100         .3....2.
-    // $0xxB=$08  00001000         3....222
+
     for x in 0..SCREEN_WIDTH as i32 {
         for y in 0..SCREEN_HEIGHT as i32 {
             let color = frame.get_pixel(x as usize, y as usize);
             assert_eq!(
                 color,
-                match (x - 9 * 8, y - 4 * 8) {
-                    (0, 1) | (1, 1) | (1, 0) | (1, 2) | (1, 3) => SYSTEM_PALETTE[3],
-                    (7, 7) | (6, 7) | (5, 7) | (6, 6) | (7, 5) | (5, 4) | (6, 4) =>
-                        SYSTEM_PALETTE[19],
-                    (0, 7) | (1, 6) | (2, 5) | (3, 4) | (4, 3) | (5, 2) | (6, 1) | (7, 0) =>
-                        SYSTEM_PALETTE[24],
-                    _ => SYSTEM_PALETTE[0],
-                }
-            )
+                wanted_palette[get_color_index_for_half_tile(x - 9 * 8, y - 4 * 8)]
+            );
         }
     }
 }
