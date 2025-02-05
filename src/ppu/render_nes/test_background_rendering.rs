948619b56d15f8f5d24a7a642437782ff80c99af
Tomer Pelleg <tomer@pelleg.org>
2025-02-05 18:27:58 +0200

    test(background drawing): added a test for the screen scrolling
    this test found a bug:(

--- src/ppu/render_nes/test_background_rendering.rs
+++ src/ppu/render_nes/test_background_rendering.rs
@@ -90,3 +90,81 @@ fn background_drawing() {
 
     assert_screen_state!(frame, 72, 32, get_color_index_for_half_tile, wanted_palette);
 }
+
+#[test]
+#[serial]
+fn test_scrolling() {
+    // this tests the scrolling
+    generate_ppu!(ppu);
+    generate_texture_canvas_event_pump!(texture, canvas, event_pump);
+
+    bus_mut!(ppu).cartridge.screen_mirroring = Mirroring::Vertical;
+
+    let our_tile = get_half_tile();
+    bus_mut!(ppu).cartridge.chr_rom = vec![0; 0x2000];
+    // again, tile 7 is our tile
+    bus_mut!(ppu).cartridge.chr_rom[7 * 16..8 * 16].copy_from_slice(&our_tile);
+
+    // tells the tile in row 17 col 23 to be our tile, in the bottom right nametable
+    // due to the mirroring, we should put it in the nametable with index 1 (top right)
+    bus_mut!(ppu).ppu_memory.vram[17 * SCREEN_WIDTH_TILE + 23 + 1024] = 7;
+
+    // now color this tile using a different palette
+    // we are in meta tile (4*4 tiles) of row 4 col 5
+    // each row has 8 meta tiles
+    bus_mut!(ppu).ppu_memory.vram[1024 + 960 + 4 * 8 + 5] = 0x0C; // we are part of the top right group
+    bus_mut!(ppu).ppu_memory.palette_table[13] = 3;
+    bus_mut!(ppu).ppu_memory.palette_table[14] = 19;
+    bus_mut!(ppu).ppu_memory.palette_table[15] = 24;
+
+    let wanted_palette = [
+        SYSTEM_PALETTE[0],
+        SYSTEM_PALETTE[3],
+        SYSTEM_PALETTE[19],
+        SYSTEM_PALETTE[24],
+    ];
+
+    // we want to add a shift
+    // let's say 350 x shift and 333 y shift
+    // remember - we should set the nametable bytes in the control if we want to add more than 256 shift
+    bus_mut!(ppu).ppu_registers.write_to_scroll(94); // 350 - 256
+    bus_mut!(ppu).ppu_registers.write_to_scroll(93); // 333 - 240
+
+    bus_mut!(ppu)
+        .ppu_registers
+        .control_register
+        .write_byte(0x03); // use the second nametable both in x and in y
+
+    let mut frame = Frame::new();
+
+    for _ in 0..2 {
+        // the y scroll only kicks into action after 1 full cycle
+        for _ in 0..SCANLINE_LENGTH_PIXELS {
+            for _ in 0..SCANLINES_PER_FRAME {
+                ppu.run_one_ppu_cycle(&mut texture, &mut frame, &mut canvas, &mut event_pump);
+            }
+        }
+    }
+
+    // before any scroll, our tile was at coordinates x = 23*8 + 256, y = 17*8 + 256
+    // when we looked at it as a tile in the bottom right nametable (mirroring!)
+    // so now it should be in y = 17*8 + 240 - 333 = 43
+    // and in x = 23 * 8 + 256 - 350 = 90
+    assert_screen_state!(frame, 90, 43, get_color_index_for_half_tile, wanted_palette);
+
+    // as an extra cherry on top (and due to a previous bug): we check that adding y nametable
+    // in vertical mirroring does not change the result
+    bus_mut!(ppu)
+        .ppu_registers
+        .control_register
+        .write_byte(0x01); // use the second nametable both in x and in y
+
+    for _ in 0..2 {
+        for _ in 0..SCANLINE_LENGTH_PIXELS {
+            for _ in 0..SCANLINES_PER_FRAME {
+                ppu.run_one_ppu_cycle(&mut texture, &mut frame, &mut canvas, &mut event_pump);
+            }
+        }
+    }
+    assert_screen_state!(frame, 90, 43, get_color_index_for_half_tile, wanted_palette);
+}
