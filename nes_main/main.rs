338a651a015cec64de85b1afa509d5c46a50f5ee
Ido Talmor <ifw.talmor@gmail.com>
2025-03-04 01:50:31 +0200

    feat(cli): started work on cli skeletone

--- nes_main/main.rs
+++ nes_main/main.rs
@@ -6,9 +6,60 @@ use nes_emulator::generate_texture_canvas_event_pump;
 use nes_emulator::ppu::frame::Frame;
 use nes_emulator::ppu::PPU;
 
+use clap::{Parser, Subcommand};
+use std::path::{Path, PathBuf};
+use std::process::exit;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    /// optional sub command
+    #[command(subcommand)]
+    command: Option<Commands>,
+
+    /// optional .nes file
+    #[arg(short = 'f', long, value_name = "game_file")]
+    #[arg(default_value = "./roms/mario.nes")]
+    game_file: PathBuf,
+
+    /// optional config file
+    #[arg(short, long, value_name = "config_file")]
+    config_file: Option<PathBuf>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// a visual configurator
+    ConfigVisual {
+        /// costum output path for the new config
+        #[clap(long, short = 'o')]
+        #[clap(default_value = "./nes_config.toml")]
+        output_path: PathBuf,
+    },
+}
+
+fn visual_config(output_file: PathBuf) {
+    todo!()
+}
+
 fn main() {
+    let cli = Cli::parse();
+    if let Some(comm) = cli.command {
+        match comm {
+            Commands::ConfigVisual { output_path } => visual_config(output_path),
+        };
+    }
+
+    if !cli.game_file.exists() {
+        panic!("path {:?} does not exist", cli.game_file);
+    }
+
+    run_emu(cli.game_file);
+}
+
+fn run_emu(game_path: PathBuf) {
     let mut bus: Bus = Bus::new();
-    let bytes = std::fs::read("./roms/mario.nes").unwrap();
+    let bytes = std::fs::read(game_path).unwrap();
     bus.cartridge.load_from_dump(&bytes);
 
     let mut bus_ref = &mut bus;
